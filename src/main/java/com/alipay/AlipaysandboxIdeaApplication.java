3c5d871a688edd19f159560920bd50f69cb15fed
siredrun <1499402309@qq.com>
2019-12-11 18:13:38 +0800

    Initial commit

---
+++ src/main/java/com/alipay/AlipaysandboxIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.alipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlipaysandboxIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlipaysandboxIdeaApplication.class, args);
+    }
+
+}
